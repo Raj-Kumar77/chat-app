d6a4f1452fa2fba8b701ad2bb4f0666ddd6265a1
Raj <raj123kumar0123@gmail.com>
2024-01-08 14:06:56 +0530

    change in server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ dotenv.config();
 connectDB();
 
 app.use(express.json()); //to accept json data
-app.use(cors());
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.send("api is running");
@@ -26,17 +26,22 @@ app.use("/api/message", messageRoutes);
 
 // ------------Deployment----------
 
-const __dirname1 = path.resolve();
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "../frontend/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "../frontend/build/index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("api is running");
-  });
-}
+// const __dirname1 = path.resolve();
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname1, "../frontend/build")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname1, "../frontend/build/index.html"));
+//   });
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("api is running");
+//   });
+// }
+
+app.use(express.static(path.join(__dirname,"../frontend/build")))
+app.get('*', function(req,res){
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+})
 
 // ------------Deployment----------
 
