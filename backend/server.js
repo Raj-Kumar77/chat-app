b5b29f78d870bd71e08a7a21c715cd1db9cee2f5
Raj <raj123kumar0123@gmail.com>
2024-01-08 14:14:35 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -26,22 +26,17 @@ app.use("/api/message", messageRoutes);
 
 // ------------Deployment----------
 
-// const __dirname1 = path.resolve();
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname1, "../frontend/build")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname1, "../frontend/build/index.html"));
-//   });
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("api is running");
-//   });
-// }
-
-app.use(express.static(path.join(__dirname,"../frontend/build")))
-app.get('*', function(req,res){
-  res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
-})
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "frontend", "build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Api is running");
+  });
+}
 
 // ------------Deployment----------
 
